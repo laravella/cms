56546e4ad2911c3ed2fdb45d1762d0a2d9f95b37
laravella <laravella.cms@gmail.com>
2013-08-27 12:33:18 +0200

    add media fields

--- src/migrations/2013_01_24_212741_create_media_table.php
+++ src/migrations/2013_01_24_212741_create_media_table.php
@@ -18,11 +18,14 @@ class CreateMediaTable extends Migration {
 			$table->string('media_type'); // IMAGE FILE | VIDEO FILE | YOUTUBE
 			$table->string('path')->nullable();
 			$table->string('caption')->default(null)->nullable();
+			$table->string('author')->default(null)->nullable();
 			$table->boolean('publish')->default(false)->nullable();
 			$table->boolean('approved')->default(false)->nullable();
 			$table->text('filedata')->nullable();
 			$table->integer('gallery_id')->unsigned()->nullable();
 			$table->string('keywords')->default(null)->nullable();
+			$table->string('mediascol')->default(null)->nullable();
+			$table->string('external_link')->default(null)->nullable();
 			$table->timestamps();
 		});
 	}
