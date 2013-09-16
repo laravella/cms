ba893ab404553f56e75b7f9a61ee3e5a1cbe07ca
laravella <laravella.cms@gmail.com>
2013-09-16 16:30:04 +0200

    crud seeder

--- src/database/seeds/CrudSeeder.php
+++ src/database/seeds/CrudSeeder.php
@@ -1,8 +1,8 @@
 <?php namespace Laravella\CMS;
 
-use Laravella\Crud\CrudSeeder;
+use Laravella\Crud\CrudSeeder as LCCrudSeeder;
 
-class CrudSeeder extends CrudSeeder {
+class CrudSeeder extends LCCrudSeeder {
 
 	/**
 	 * Run the database seeds.
@@ -16,13 +16,13 @@ class CrudSeeder extends CrudSeeder {
             $displayTypeId = $this->getId('_db_display_types', 'name', 'widget');
             $widgetTypeId = $this->getId('_db_widget_types', 'name', 'thumbnail');
             $this->updateOrInsert('_db_fields', array('id'=>$mediaId), 
-                    array('display_type_id'=>$displayTypeId, 'widgetTypeId'=>$widgetTypeId));
+                    array('display_type_id'=>$displayTypeId, 'widget_type_id'=>$widgetTypeId));
             
             //set contents.content to ckeditor
             $contentId = $this->getId('_db_fields', 'fullname', 'contents.content');
             $widgetTypeId = $this->getId('_db_widget_types', 'name', 'ckeditor');
             $this->updateOrInsert('_db_fields', array('id'=>$contentId), 
-                    array('display_type_id'=>$displayTypeId, 'widgetTypeId'=>$widgetTypeId));
+                    array('display_type_id'=>$displayTypeId, 'widget_type_id'=>$widgetTypeId));
 	}
 
 }
